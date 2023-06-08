3dc97ced26dcd96669c2962194dadf8d120ad758
Pushkar <pushkaranmol100@gmail.com>
2023-06-09 01:51:50 +0530

    ok

---
+++ client/build/precache-manifest.6975d6b4bcd9a87e48f7c8b7eed49231.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7007de628ecbe6456a386311926461b5",
+    "url": "/Memories/index.html"
+  },
+  {
+    "revision": "824f98bb09d2a1650082",
+    "url": "/Memories/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/Memories/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/Memories/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824f98bb09d2a1650082",
+    "url": "/Memories/static/js/main.264ea3f1.chunk.js"
+  },
+  {
+    "revision": "eaa39524d7dd2796a648",
+    "url": "/Memories/static/js/runtime-main.4e7846fa.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/Memories/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/Memories/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
